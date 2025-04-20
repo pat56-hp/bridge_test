6855f9a4fa3a79895336519fa8e583afee3dbfac
pat56-hp <patrickkouassi7@gmail.com>
2025-04-20 08:56:10 +0000

    update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,10 @@ composer install --no-interaction --prefer-dist --optimize-autoloader
 php artisan key:generate
 
 # Exécute les migrations (optionnel, si tu veux)
-# php artisan migrate --force
+php artisan migrate --force
+
+# Exécute les seeders (optionnel, si tu veux)
+php artisan db:seed --force
 
 # Sert l'application Laravel sur le bon port
 php artisan serve --host=0.0.0.0 --port=$PORT
